58abfab7a6437f65f1aa12655b578fa3298e6cfd
Michael Ogaye <hogaeymic@gmail.com>
2022-04-08 23:28:27 +0300

    created the quotes array and displayed it in the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,10 @@ export class AppComponent {
 
   allQuotes:Quote[]=[
     new Quote(
-      'medicine','Michael','If life were predictable it would cease to be life, and be without flavor.',0,0,'2022-04-08'
+      'medicine','Michael','If life were predictable  it would cease to be life, and be without flavor.',0,0,'2022-04-08'
     ),
     new Quote(
-      'Justice','Justus','If life were predictable it would cease to be life, and be without flavor.',0,0,'2022-04-08'
+      'Justice','Justus','Descriptions are like skirts, they should be long enough to cover the subject, but short enough to keep things interesting.',0,0,'2022-04-08'
     ),
     new Quote(
       'knowlege','Paul','If life were predictable it would cease to be life, and be without flavor.',0,0,'2022-04-08'
@@ -29,4 +29,15 @@ export class AppComponent {
 log(){
   console.log(this.allQuotes)
 }
+// method to add new quotes into the array
+addQuote(dat:Quote){
+let title=dat.title;
+let author= dat.author;
+let cont=dat.content;
+let date=dat.date;
+this.allQuotes.push()
+
+}
+
+url1="../assets/images/right.svg"
 }
