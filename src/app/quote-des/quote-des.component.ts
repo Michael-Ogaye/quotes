11d87284b99ae7cd4a48c1f64150a8b9b01dea47
Michael Ogaye <hogaeymic@gmail.com>
2022-04-09 23:42:19 +0300

    pt:modified the Quote class

--- src/app/quote-des/quote-des.component.ts
+++ src/app/quote-des/quote-des.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-des',
   templateUrl: './quote-des.component.html',
   styleUrls: ['./quote-des.component.css']
 })
 export class QuoteDesComponent implements OnInit {
-
+@Input()quote:Quote | undefined
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,9 @@
 import { Title } from "@angular/platform-browser";
 
 export class Quote {
+    showDes!: boolean;
 
     constructor(public title:string, public content:string,public author:string,public sub:string,public date:Date,public upVote:number,public downVote:number){
-
+this.showDes=false;
     }
 }
