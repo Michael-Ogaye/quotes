552bea964e5b7c5e65ea396cd47b44c22645df0b
Michael Ogaye <hogaeymic@gmail.com>
2022-04-09 16:15:46 +0300

    created a custom directive to highlight quote with highest number of upvotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,14 @@
+import { Directive,ElementRef,OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnInit {
+
+  constructor(private elem:ElementRef) { }
+ngOnInit(): void {
+    this.elem.nativeElement.style.backgroundColor='blue';
+}
+
+
+}
